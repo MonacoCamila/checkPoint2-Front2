1f8f90409a2278cce89b7819e3816dc0488423ad
MonacoCamila <camilamonaco@gmail.com>
2022-09-26 18:33:33 -0300

    add tarefas.js

--- ToDoApp/scripts/tarefas.js
+++ ToDoApp/scripts/tarefas.js
@@ -1,4 +1,83 @@
-window.onload = () => {
+let tokenJwt;
+
+//executa automaticamente ao chamar a pagina tarefas.html
+
+onload = function(){
+
+    //busca o token no storage 
+    tokenJwt = localStorage.getItem('token');
+
+    if(!tokenJwt) {
+        //caso o token nao esteja no storage
+        this.window.location = 'index.html';
+    } else {
+        //caso o token esteja valido 
+        //buscar dados do usuario
+        buscaDadosUsuario();
+
+    }
+}
+
+async function buscaDadosUsuario() {
+
+    try{
+
+        let resposta = await fetch('https://ctd-fe2-todo-v2.herokuapp.com/v1/users/getMe', {
+                method: 'GET',
+                headers:{
+                    'authorization': tokenJwt
+                }
+            });
+
+            if(resposta.status==200 || resposta.status==201) {
+            let respostaJs = await resposta.json();
+            console.log(respostaJs);
+            }
+            
+            manipulaDadosUsuario(respostaJS);
+            
+            else {
+                throw "ocorreu algum erro"
+            } 
+                  
+
+            
+    } catch (error){
+        alert(error)
+        console.log(error);    
+
+}
+
+let manipulaDadosUsuario = (dadosRecebidos)=>{
+    console.log(dadosRecebidos);
+
+    let nomeUsuario = document.getElementById("nomeUsuario");
+
+    //nomeUsuario
+    nomeUsuario.innerText = `${dadosRecebidos.firstName} ${dadosRecebidos.lastName}`;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*window.onload = () => {
     getUser();
     getTarefas();
 };
@@ -44,4 +123,4 @@ function getTarefas() {
         console.log(err)
     })
 
-}
+}*/
