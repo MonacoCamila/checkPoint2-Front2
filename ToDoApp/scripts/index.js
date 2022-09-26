a786644becc2d1e274860e14eda553bdbb2f2e9c
MonacoCamila <camilamonaco@gmail.com>
2022-09-26 16:58:14 -0300

    mais alteracoes no index.html

--- ToDoApp/scripts/index.js
+++ ToDoApp/scripts/index.js
@@ -1,4 +1,4 @@
-async function login(event) {
+function login(event) {
   event.preventDefault();
 
   //pegar infos do formulario 
@@ -9,6 +9,11 @@ async function login(event) {
   const data ={
       email,
       password
+  }
+
+  let loginUsuario = {
+    email:'',
+    password:''
   }
 
   // variaveis de controle para validacoes do form login 
@@ -24,6 +29,11 @@ async function login(event) {
     //busca valores atualizados do login 
     emailLogin = document.getElementById('email');
     passwordLogin = document.getElementById('password');
+
+    // retira os espacos vazios 
+    function normalizaUsandoTrim(textoRecebido) {
+        return  textoRecebido.trim()
+    }
 
     if(validaLogin()){
         evento.preventDefault();
@@ -50,17 +60,35 @@ async function login(event) {
 
   });
 
+  // verifica se ambas informacoes estao validas 
 
-  //fazer login do usuario na API  
+  function validaLogin(){
+    if(emailValido && passwordValido){
 
-  const response = await fetch('https://ctd-fe2-todo-v2.herokuapp.com/v1/users/login', {
+    //ativa o botao de login 
+    botaoLogin.innerText = 'Acessar';
+    botaoLogin.removeAttribute('disabled');
+    return true;
+
+
+  } else {
+    console.log('login invalido');
+  }
+
+}
+  
+//fazer login do usuario na API  
+
+const response = fetch('https://ctd-fe2-todo-v2.herokuapp.com/v1/users/login', {
       method: 'POST',
       headers: {
           'content-type': 'application/json',
       },
       body: JSON.stringify(data)
+
   }).then((response) => {
       return response.json();
+      
   }).catch((error) => {
       console.log(error);
   })
@@ -74,43 +102,4 @@ async function login(event) {
   window.location.href = 'tarefas.html';
 
 }
-
-/*Validações necessárias:
-
-repetir a senha deve corresponder à senha (register)
-
-nenhum campo vazio (register e login)
-
-devem finalmente preparar o objeto normalizado em ambos os casos. Exemplo para register:        
-      {
-        "firstName": "string",
-        "lastName": "string",
-        "email": "string",
-        "password": "string"
-      }
-
-
-Desenvolver o signup.js (script que deve estar ligado em signup.html)
-Aqui, você deve usar um evento para lançar o POST correspondente à API, processo através do qual se realiza a criação de um usuário, exibindo finalmente a resposta no console.
-
-Desenvolver o index.js (script que deve estar ligado em index.html)
-Aqui, você deve usar um evento para lançar o POST correspondente à API. Neste caso, o processo refere-se à situação em que um usuário efetua login na aplicação, finalmente exibindo a resposta no console.
-
-Nesta fase do projeto, você conseguirá: 
-a) capturar, manipular, normalizar e validar os dados informados pelo usuário, nas telas de login e cadastro; 
-b) consumir a API To-Do, cadastrando um novo usuário e realizando o login com o usuário cadastrado; 
-c) exibir as informações retornadas pela API no console da sua aplicação;
-d) armazenar o token jwt de acesso retornado pela API ao fazer o login, usando os recursos de armazenamento do HTML e JS.
-
-
-*/
-
-
-
-
-
-
-
-
-
 
